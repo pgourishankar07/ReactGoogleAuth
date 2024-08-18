786b4fab0b3fc32cffd17e071f1fddd9ef36e22e
Gouri Shankar <gourishankar@Gouris-MacBook-Air.local>
2024-08-18 13:13:35 +0530

    complete sigin page with google and removed unecessary codes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="337691363377-g81o8q5f29aat94sd0805pae7p8qr0pm.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>
 );
